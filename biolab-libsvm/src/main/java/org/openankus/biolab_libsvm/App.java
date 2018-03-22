dc1d57354a708f5f9af4e61e8ab0e635dd2f1a76
prismdata <prismdata@prismdata-PC>
2018-03-22 22:33:00 +0900

    initial commit

---
+++ biolab-libsvm/src/main/java/org/openankus/biolab_libsvm/App.java
@@ -0,0 +1,13 @@
+package org.openankus.biolab_libsvm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
